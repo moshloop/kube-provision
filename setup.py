c2bec67cd272e6454c1752aa9ddf245ff83e5703
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-21 08:57:39 +0200

    0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name = 'kube-provision',
-    version = '0.1',
+    version = '0.2',
     scripts = ['kube-provision'],
     install_requires = ['ansible-deploy', 'ansible-provision', 'kubectl-ansible'],
     url = 'https://www/github.com/moshloop/kube-provision',
